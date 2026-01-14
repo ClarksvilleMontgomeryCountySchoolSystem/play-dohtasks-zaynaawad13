0697c03d1826685d4b93384947a9b8180bd7991f
zaynaawad13 <zayna.awad725@students.cmcss.net>
2026-01-14 07:58:29 -0600

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,36 @@
+"""
+ANSWER BOX - Copy the strings you need:
 
+"long tail or short tail?"
+
+") Add four small legs to the bottom using {color1}.\n"
+
+") Name this creation: "Mouse""
+
+
+"""
+
+def main():
+    color1 = "gray"
+    color2 = "pink"
+    print(f"1) Roll a ball using {color1}.\n")
+    choice1 = input("long body or round body? ")
+    # CAUTION: You must include the word "body" when checking!
+    if choice1 == "long body":
+        print("2) Roll the ball into an egg shape.\n")
+    else:
+        print("2) Keep it as a ball.\n")
+    print("3) Roll a smaller ball using {color1} for the head.\n")
+    print("4) Attach the head to one end of the body.\n")
+    choice2 = input("long tail or short tail? ")
+    if choice2 == "long tail":
+        print("5) Roll a thin rope using {color2} and attach to the back.\n")
+    else:
+        print("5) Add a small bump using {color2} to the back.\n")
+    print(f"6) Add four small legs to the bottom using {color1}.\n")
+    print("7) Add two dots for eyes and a tiny nose.\n")
+    print("8) Name this creation: 'Mouse'")
+
+
+if __name__ == "__main__":
+    main()
